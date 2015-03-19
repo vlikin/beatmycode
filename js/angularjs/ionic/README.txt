299b775c35c7d25e42bcb96f070f2f8c4a0a3bcb
Viktor Likin <viktor.likin@gmail.com>
2015-03-19 15:41:44 +0200

    Build an App with Navigation and Routing - Part 2 has been finished.

--- js/angularjs/ionic/README.txt
+++ js/angularjs/ionic/README.txt
@@ -1,3 +1,6 @@
 Agenda
 
 1. Routing - http://learn.ionicframework.com/formulas/navigation-and-routing-part-1/ also -2/
+
+=== FAQ ===
+Abstract states provide the structure necessary to handle advanced features like view nesting.
--- js/angularjs/ionic/Routing/www/js/app.js
+++ js/angularjs/ionic/Routing/www/js/app.js
@@ -1,24 +1,72 @@
+// Code goes here
+
 var app = angular.module('ionicApp', ['ionic'])
 
 app.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/')
+  $urlRouterProvider.otherwise('/todos')
 
-  $stateProvider.state('home', {
-    url: '/home',
+  $stateProvider.state('app', {
+    abstract: true,
+    templateUrl: 'main.html'
+  })
+
+  $stateProvider.state('app.todos', {
+    abstract: true,
+    url: '/todos',
     views: {
-      home: {
-        templateUrl: 'home.html'
+      todos: {
+        template: '<ion-nav-view></ion-nav-view>'
       }
     }
-  });
+  })
 
-  $stateProvider.state('help', {
+    $stateProvider.state('app.todos.index', {
+    url: '',
+    templateUrl: 'todos.html',
+    controller: 'TodosCtrl'
+  })
+
+  $stateProvider.state('app.todos.detail', {
+    url: '/:todo',
+    templateUrl: 'todo.html',
+    controller: 'TodoCtrl',
+    resolve: {
+      todo: function($stateParams, TodosService) {
+        return TodosService.getTodo($stateParams.todo)
+      }
+    }
+  })
+
+
+  $stateProvider.state('app.help', {
     url: '/help',
     views: {
       help: {
         templateUrl: 'help.html'
       }
     }
-  });
+  })
+})
 
+app.factory('TodosService', function() {
+  var todos = [
+      {title: "Take out the trash", done: true},
+      {title: "Do laundry", done: false},
+      {title: "Start cooking dinner", done: false}
+   ]
+
+  return {
+    todos: todos,
+    getTodo: function(index) {
+      return todos[index]
+    }
+  }
+})
+
+app.controller('TodosCtrl', function($scope, TodosService) {
+  $scope.todos = TodosService.todos
+})
+
+app.controller('TodoCtrl', function($scope, todo) {
+  $scope.todo = todo
 })
